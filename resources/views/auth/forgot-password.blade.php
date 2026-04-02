2bf62bb47b1f9e711915bdd16568c0b8c10d4b23
ku100na <ku1009.na9901@gmail.com>
2026-04-02 20:59:47 +0000

    feat: パスワード再設定機能を追加
    - パスワードリセットメール送信
    - パスワード再設定画面の作成
    - パスワードリセット画面の作成
    - エラーメッセージ・成功通知の表示

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,39 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="font-bold text-xl pb-2">
+            パスワードリセット
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <div class="p-6">
+        <div class="mb-4 text-sm text-gray-600">
+            登録したメールアドレスを入力してください。<br>
+            リセット用のリンクを送信します。
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+        <form method="POST" action="{{ route('password.email') }}" novalidate>
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" value="メールアドレス" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <div class="mt-4">
+                <x-primary-button>
+                    送信
+                </x-primary-button>
+            </div>
+
+            <div class="mt-4">
+                <a class="underline hover:text-indigo-500"
+                            href="{{ route('login') }}">
+                                ログインに戻る
+                            </a>
+            </div>
+        </form>
+    </div>
 </x-guest-layout>
--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -4,6 +4,12 @@
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
+
+            @if ($message === __('passwords.token')) 
+                <a href="{{ route('password.request') }}" class="underline text-primary01 hover:text-indigo-500">
+                    もう一度リクエストする
+                </a> 
+            @endif 
         @endforeach
     </ul>
 @endif
